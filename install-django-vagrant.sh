1151ce120c408b0c815767156d9db7ebd6fe983f
Brendon Edmiston <bedmiston@gmail.com>
2014-11-06 12:01:44 -0600

    Remove unnecessary packages from host vm

--- install-django-vagrant.sh
+++ install-django-vagrant.sh
@@ -1,10 +1,5 @@
  #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y git build-essential python libpq-dev python-dev python-setuptools python-pip
-pip install virtualenv
-mkdir /reynolds
-virtualenv /reynolds
-cd /reynolds
-source /reynolds/bin/activate
+apt-get install -y python-pip
 pip install -U fig
