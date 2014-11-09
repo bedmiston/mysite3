be229469869e5c5eb46e0510cfa42d42d3c1156d
Brendon Edmiston <bedmiston@gmail.com>
2014-11-09 13:56:20 -0600

    Improve models admin

--- app/polls/admin.py
+++ app/polls/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from polls.models import Choice, Question
 
-# Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        ]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
