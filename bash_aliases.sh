4337af08934aac5dbaee9c89f0c7ecd5f901e495
Brendon Edmiston <bedmiston@gmail.com>
2014-11-15 10:35:29 -0600

    Change to use phusion/baseimage so we can ssh into the container. This is so we can debug using PyCharm.

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -24,7 +24,7 @@ drm() { docker rm $(docker ps -a -q); }
 alias drmf='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
 # Remove all images
 dri() { docker rmi $(docker images -q); }
-# Dockerfile build, e.g., $dbu tcnksm/test
+# python34.Dockerfile build, e.g., $dbu tcnksm/test
 dbu() { docker build -t=$1 .; }
 # Show all alias related docker
 dalias() { alias | grep 'docker' | sed "s/^\([^=]*\)=\(.*\)/\1 => \2/"| sed "s/['|\']//g" | sort; }
